37b07477e5d31ebd8e3e2a810cf92f580f11868b
Digicat195 <96354124+Digicat195@users.noreply.github.com>
2023-04-25 19:34:13 +0100

    Created Order Form

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -23,3 +23,15 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
     }, 10);
 }
 
+// Dealing with Textarea Height
+function calcHeight(value) {
+    let numberOfLineBreaks = (value.match(/\n/g) || []).length;
+    // min-height + lines x line-height + padding + border
+    let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
+    return newHeight;
+  }
+  
+  let textarea = document.querySelector(".resize-ta");
+  textarea.addEventListener("keyup", () => {
+    textarea.style.height = calcHeight(textarea.value) + "px";
+  });
